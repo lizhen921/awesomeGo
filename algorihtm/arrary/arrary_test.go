1d6fc5c50f74676280b29b53905eeef6f70c54a1
lizhen <lizhen10762@autohome.com.cn>
2021-11-08 14:18:45 +0800

    移除数组元素

--- algorihtm/arrary/arrary_test.go
+++ algorihtm/arrary/arrary_test.go
@@ -44,6 +44,49 @@ func TestBinarySearch(t *testing.T) {
 	fmt.Println(index)
 }
 
+/*
+题目：https://leetcode-cn.com/problems/remove-element/
+
+给你一个数组 nums和一个值val，你需要原地移除所有数值等于val的元素，并返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+
+元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+
+比如：
+
+输入：nums = [3,2,2,3], val = 3
+
+输出：2, nums = [2,2]
+
+解释：函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。你不需要考虑数组中超出新长度后面的元素。例如，函数返回的新长度为 2 ，而 nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案。
+
+输入：nums = [0,1,2,2,3,0,4,2], val = 2
+
+输出：5, nums = [0,1,4,0,3]
+
+解释：函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。注意这五个元素可为任意顺序。你不需要考虑数组中超出新长度后面的元素。
+
+*/
+
+func removeElement(nums []int, val int) int {
+	start := 0
+
+	for i, v := range nums {
+		if v != val {
+			nums[start] = nums[i]
+			start++
+		}
+	}
+	return start
+}
+
+func TestRemoveElement(t *testing.T) {
+	nums := []int{0, 1, 2, 2, 3, 0, 4, 2}
+	numsLen := removeElement(nums, 2)
+	fmt.Println(numsLen, nums)
+}
+
 func TestArrary(t *testing.T) {
 	arr := []int{1, 2, 3, 4, 5, 6}
 	fmt.Printf("%v\n", &arr)
