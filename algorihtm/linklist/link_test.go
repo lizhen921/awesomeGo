21d9029589580a511bc4c1b8dccd4656c34863fb
Li Zhen <294474470@qq.com>
2021-11-10 23:20:20 +0800

    go format

--- algorihtm/linklist/link_test.go
+++ algorihtm/linklist/link_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 type ListNode struct {
-	Val int
+	Val  int
 	Next *ListNode
 }
 
@@ -16,13 +16,13 @@ func GengrateLinkList(valList []int) *ListNode {
 
 	for i := 0; i < len(valList); i++ {
 		node := &ListNode{
-			Val: valList[i],
+			Val:  valList[i],
 			Next: nil,
 		}
 		if tail == nil {
 			tail = node
 			head = tail
-		}else {
+		} else {
 			tail.Next = node
 			tail = node
 		}
@@ -30,19 +30,20 @@ func GengrateLinkList(valList []int) *ListNode {
 	return head
 }
 
-func PrintLinkList(head *ListNode)  {
+func PrintLinkList(head *ListNode) {
 	for head != nil {
-		fmt.Printf("%d->",head.Val)
+		fmt.Printf("%d->", head.Val)
 		head = head.Next
 	}
 	fmt.Println("")
 }
 
-func TestGengrateLinkList(t *testing.T)  {
-	valList := []int{3,4,6,1,7,2}
+func TestGengrateLinkList(t *testing.T) {
+	valList := []int{3, 4, 6, 1, 7, 2}
 	head := GengrateLinkList(valList)
 	PrintLinkList(head)
 }
+
 /*
 给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回新的头节点 。
 
@@ -53,20 +54,20 @@ func removeElements(head *ListNode, val int) *ListNode {
 	preNode := &ListNode{
 		Next: head,
 	}
-	for tempHead := preNode;tempHead.Next != nil; {
+	for tempHead := preNode; tempHead.Next != nil; {
 		if tempHead.Next.Val == val {
 			tempHead.Next = tempHead.Next.Next
-		}else {
+		} else {
 			tempHead = tempHead.Next
 		}
 	}
 	return preNode.Next
 }
 
-func TestRemoveElements(t *testing.T)  {
-	valList := []int{7,7,7,8}
+func TestRemoveElements(t *testing.T) {
+	valList := []int{7, 7, 7, 8}
 	head := GengrateLinkList(valList)
 	PrintLinkList(head)
-	head = removeElements(head,7)
+	head = removeElements(head, 7)
 	PrintLinkList(head)
 }
