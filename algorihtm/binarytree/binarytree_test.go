f7a225100f87bc2635f85c7fd99a32d797479557
lizhen <lizhen10762@autohome.com.cn>
2021-11-16 18:32:56 +0800

    二叉树最大深度

--- algorihtm/binarytree/binarytree_test.go
+++ algorihtm/binarytree/binarytree_test.go
@@ -1,1 +1,26 @@
 package binarytree
+
+/*
+给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例：
+给定二叉树 [3,9,20,null,null,15,7]，
+
+*/
+func maxDepth0(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	leftDeep := maxDepth0(root.Left)
+	rightDeep := maxDepth0(root.Right)
+	if leftDeep > rightDeep {
+		return leftDeep + 1
+	}
+
+	return rightDeep + 1
+}
