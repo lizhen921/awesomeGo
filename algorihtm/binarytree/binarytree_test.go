d540a1733b68040928402dda01fd0ea63bf49b30
Li Zhen <294474470@qq.com>
2021-11-16 23:12:15 +0800

    递归判断是否平衡二叉树

--- algorihtm/binarytree/binarytree_test.go
+++ algorihtm/binarytree/binarytree_test.go
@@ -1,5 +1,11 @@
 package binarytree
 
+import (
+	"fmt"
+	"math"
+	"testing"
+)
+
 /*
 给定一个二叉树，找出其最大深度。
 
@@ -23,4 +29,51 @@ func maxDepth0(root *TreeNode) int {
 	}
 
 	return rightDeep + 1
+}
+
+/*
+给定一个二叉树，判断它是否是高度平衡的二叉树。
+
+本题中，一棵高度平衡二叉树定义为：
+
+一个二叉树每个节点的左右两个子树的高度差的绝对值不超过 1 。
+*/
+
+//自上而下 递归
+/*
+结束条件： root==nil
+返回值：left right  depth<1
+*/
+func isBalanceTree(root *TreeNode) bool {
+	isB,_ := isBalance(root)
+	return isB
+}
+//返回当前树是否平衡二叉树，和当前树的深度
+func isBalance(root *TreeNode) (bool,int) {
+	if root == nil{
+		return true,0
+	}
+
+	left, leftDepth := isBalance(root.Left)
+	right,rightDepth := isBalance(root.Right)
+
+	depth := leftDepth
+	if leftDepth < rightDepth {
+		depth = rightDepth
+	}
+
+	return left && right && math.Abs(float64(leftDepth-rightDepth))<=1 , depth+1
+}
+
+
+//自下而上 递归
+func isBalanceTree2(root *TreeNode) bool {
+
+	return false
+}
+
+func TestIsBalance(t *testing.T)  {
+	root := NewTree2()
+	isB := isBalanceTree(root)
+	fmt.Println(isB)
 }
--- algorihtm/binarytree/maxdepth_test.go
+++ algorihtm/binarytree/maxdepth_test.go
@@ -42,3 +42,28 @@ func NewTree() *TreeNode {
 
 	return n1
 }
+func NewTree2() *TreeNode {
+	n1 := &TreeNode{1, nil, nil}
+	n2 := &TreeNode{2, nil, nil}
+	n3 := &TreeNode{3, nil, nil}
+	n4 := &TreeNode{4, nil, nil}
+	n5 := &TreeNode{5, nil, nil}
+	n6 := &TreeNode{6, nil, nil}
+	n7 := &TreeNode{7, nil, nil}
+	n8 := &TreeNode{8, nil, nil}
+	n9 := &TreeNode{9, nil, nil}
+	n10 := &TreeNode{10, nil, nil}
+	n11 := &TreeNode{11, nil, nil}
+	n1.Left = n2
+	n1.Right = n3
+	n2.Left = n4
+	n2.Right = n5
+	n3.Left = n6
+	n3.Right = n7
+	n4.Left = n8
+	n4.Right = n9
+	n6.Left = n10
+	n6.Right = n11
+
+	return n1
+}
