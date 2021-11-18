d7cfdf9c6fad5107358f238fbba5af03cf42473d
lizhen <lizhen10762@autohome.com.cn>
2021-11-18 18:10:17 +0800

    递归计算二叉树最小深度

--- algorihtm/binarytree/binarytree_test.go
+++ algorihtm/binarytree/binarytree_test.go
@@ -101,3 +101,35 @@ func symmetricNode(leftTree *TreeNode, rightTree *TreeNode) bool {
 	}
 	return leftTree.Val == rightTree.Val && symmetricNode(leftTree.Left, rightTree.Right) && symmetricNode(leftTree.Right, rightTree.Left)
 }
+
+/*
+给定一个二叉树，找出其最小深度。
+
+最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+
+说明：叶子节点是指没有子节点的节点。
+
+*/
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+
+	lD, rD := math.MaxInt32, math.MaxInt32
+	if root.Left != nil {
+		lD = minDepth(root.Left)
+	}
+
+	if root.Right != nil {
+		rD = minDepth(root.Right)
+	}
+
+	if lD > rD {
+		return rD + 1
+	}
+	return lD + 1
+}
