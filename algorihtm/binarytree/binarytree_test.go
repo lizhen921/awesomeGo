d313f8c8668ef4aef525d46a8dbab7c8e5859d62
lizhen <lizhen10762@autohome.com.cn>
2021-11-18 16:46:22 +0800

    递归判断是否对称二叉树

--- algorihtm/binarytree/binarytree_test.go
+++ algorihtm/binarytree/binarytree_test.go
@@ -71,3 +71,33 @@ func TestIsBalance(t *testing.T) {
 	isB := isBalanceTree(root)
 	fmt.Println(isB)
 }
+
+/*
+对称二叉树
+
+给定一个二叉树，检查它是否是镜像对称的。
+
+例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+
+*/
+
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	leftTree := root.Left
+	rightTree := root.Right
+
+	isSym := symmetricNode(leftTree, rightTree)
+	return isSym
+}
+
+func symmetricNode(leftTree *TreeNode, rightTree *TreeNode) bool {
+	if leftTree == nil && rightTree == nil {
+		return true
+	}
+	if leftTree == nil || rightTree == nil {
+		return false
+	}
+	return leftTree.Val == rightTree.Val && symmetricNode(leftTree.Left, rightTree.Right) && symmetricNode(leftTree.Right, rightTree.Left)
+}
