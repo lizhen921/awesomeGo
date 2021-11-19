f3ea4ff589771b267a2f9cb7fc480841224a3aee
lizhen <lizhen10762@autohome.com.cn>
2021-11-19 15:13:50 +0800

    递归删除链表重复元素

--- algorihtm/linklist/link_test.go
+++ algorihtm/linklist/link_test.go
@@ -290,13 +290,35 @@ func swappairs2(head *ListNode) *ListNode {
 	return tail
 }
 
-
 func TestSwapPairs(t *testing.T) {
 	valList := []int{3, 4, 6, 1, 7, 2, 1}
 	head := GengrateLinkList(valList)
 	PrintLinkList(head)
 	head = swappairs2(head)
 	PrintLinkList(head)
+}
+
+/*
+存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+
+返回同样按升序排列的结果链表。
+
+输入：head = [1,1,2]
+输出：[1,2]
+*/
+func deleteDuplicates(head *ListNode) *ListNode {
+
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	head.Next = deleteDuplicates(head.Next)
+
+	if head.Val == head.Next.Val {
+		head = head.Next
+	}
+
+	return head
 }
 
 /*
