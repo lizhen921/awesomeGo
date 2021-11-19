cb2f44811191b886ee2246a7f5030b2fd63d8f42
lizhen <lizhen10762@autohome.com.cn>
2021-11-19 14:16:53 +0800

    递归构建二叉树

--- algorihtm/binarytree/binarytree_test.go
+++ algorihtm/binarytree/binarytree_test.go
@@ -225,3 +225,43 @@ func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
 	}
 	return root
 }
+
+/*
+给定一个不含重复元素的整数数组 nums 。一个以此数组直接递归构建的 最大二叉树 定义如下：
+
+二叉树的根是数组 nums 中的最大元素。
+左子树是通过数组中 最大值左边部分 递归构造出的最大二叉树。
+右子树是通过数组中 最大值右边部分 递归构造出的最大二叉树。
+返回有给定数组 nums 构建的 最大二叉树 。
+
+*/
+
+func constructMaximumBinaryTree(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	index := findMax(nums)
+	root := &TreeNode{Val: nums[index]}
+	root.Left = constructMaximumBinaryTree(nums[:index])
+	root.Right = constructMaximumBinaryTree(nums[index+1:])
+	return root
+}
+
+func findMax(nums []int) (index int) {
+	for i := 0; i < len(nums); i++ {
+		if nums[i] > nums[index] {
+			index = i
+		}
+	}
+	return
+}
+
+func TestConstructMaximumBinaryTree(t *testing.T) {
+	nums := []int{3, 2, 1, 6, 0, 5, 9}
+
+	tree := constructMaximumBinaryTree(nums)
+
+	fmt.Println(tree)
+
+}
