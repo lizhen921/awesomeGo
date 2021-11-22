da1043236456f4267972df393db7159bcc66b01b
Li Zhen <294474470@qq.com>
2021-11-22 23:21:10 +0800

    判断链表有没有环

--- algorihtm/linklist/link_test.go
+++ algorihtm/linklist/link_test.go
@@ -376,6 +376,61 @@ func removeNthFromEnd2(head *ListNode, n int) *ListNode {
 	}
 	return dummy.Next
 }
+/*
+给你一个链表的头节点 head ，判断链表中是否有环。
+
+如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+
+如果链表中存在环，则返回 true 。 否则，返回 false 。
+输入：head = [3,2,0,-4], pos = 1
+输出：true
+解释：链表中有一个环，其尾部连接到第二个节点。
+*/
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	a := head
+	b := head.Next
+	for a != b {
+		if b == nil || b.Next == nil{
+			return false
+		}
+		a = a.Next
+		b = b.Next.Next
+	}
+	return true
+}
+
+/*
+判断有没有环，并找到入口
+
+输入：head = [3,2,0,-4], pos = 1
+输出：返回索引为 1 的链表节点
+解释：链表中有一个环，其尾部连接到第二个节点。
+
+*/
+func detectCycle(head *ListNode) *ListNode {
+	headMap := make(map[*ListNode]int)
+	for head != nil {
+		if _,ok := headMap[head];ok {
+			return head
+		}else {
+			headMap[head] = 1
+			head = head.Next
+		}
+	}
+	return nil
+}
+
+
 
 /*
 
