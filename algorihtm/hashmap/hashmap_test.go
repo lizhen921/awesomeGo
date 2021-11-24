9eb10e9a461f2bcce781b3cb3519f8dfd5f4c777
lizhen <lizhen10762@autohome.com.cn>
2021-11-24 18:50:21 +0800

    快乐数

--- algorihtm/hashmap/hashmap_test.go
+++ algorihtm/hashmap/hashmap_test.go
@@ -1,8 +1,10 @@
 package hashmap
 
 import (
+	"fmt"
 	"sort"
 	"strings"
+	"testing"
 )
 
 /*
@@ -98,6 +100,55 @@ func intersection2(nums1 []int, nums2 []int) []int {
 		}
 
 	}
-
 	return num
+}
+
+/*
+编写一个算法来判断一个数 n 是不是快乐数。
+
+「快乐数」定义为：
+
+对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+如果 可以变为  1，那么这个数就是快乐数。
+如果 n 是快乐数就返回 true ；不是，则返回 false 。
+
+示例 1：
+
+输入：n = 19
+输出：true
+解释：
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+示例 2：
+
+输入：n = 2
+输出：false
+
+*/
+func isHappy(n int) bool {
+	nMap := make(map[int]bool)
+	for n != 1 {
+		n = getNext(n)
+		if nMap[n] {
+			return false
+		}
+		nMap[n] = true
+	}
+	return true
+}
+
+func getNext(n int) int {
+	var res int
+	for n > 0 {
+		res += (n % 10) * (n % 10)
+		n = n / 10
+	}
+	return res
+}
+
+func TestIsHappy(t *testing.T) {
+	fmt.Println(isHappy(19))
 }
