52921d2b1e49519e3a411858a1a6603c8f29a3fa
lizhen <lizhen10762@autohome.com.cn>
2021-11-24 17:30:16 +0800

    字符串互为字母异位词

---
+++ algorihtm/hashmap/hashmap_test.go
@@ -0,0 +1,49 @@
+package hashmap
+
+import (
+	"sort"
+	"strings"
+)
+
+/*
+给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+
+注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。
+
+输入: s = "anagram", t = "nagaram"
+输出: true
+
+输入: s = "rat", t = "car"
+输出: false
+*/
+
+func isAnagram(s string, t string) bool {
+	sliceS := strings.Split(s, "")
+	sliceT := strings.Split(t, "")
+	sort.Strings(sliceS)
+	sort.Strings(sliceT)
+	s = strings.Join(sliceS, "")
+	t = strings.Join(sliceT, "")
+	if s == t {
+		return true
+	}
+	return false
+}
+
+func isAnagram2(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	strMap := make(map[rune]int)
+	for _, v := range s {
+		strMap[v]++
+	}
+
+	for _, v := range t {
+		strMap[v]--
+		if strMap[v] < 0 {
+			return false
+		}
+	}
+	return true
+}
