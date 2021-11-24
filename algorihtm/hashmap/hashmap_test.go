629fcc48ad19440174569fc23b08fc0753041ad4
lizhen <lizhen10762@autohome.com.cn>
2021-11-24 18:11:01 +0800

    计算两个数组的交集

--- algorihtm/hashmap/hashmap_test.go
+++ algorihtm/hashmap/hashmap_test.go
@@ -47,3 +47,57 @@ func isAnagram2(s string, t string) bool {
 	}
 	return true
 }
+
+/*
+给定两个数组，编写一个函数来计算它们的交集。
+
+示例 1：
+
+输入：nums1 = [1,2,2,1], nums2 = [2,2]
+输出：[2]
+
+示例 2：
+
+输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出：[9,4]
+*/
+
+func intersection(nums1 []int, nums2 []int) []int {
+	numMap := make(map[int]int)
+	num := make([]int, 0, 0)
+	for _, v := range nums1 {
+		numMap[v] = 1
+	}
+
+	for _, v := range nums2 {
+		if n, ok := numMap[v]; ok && n == 1 {
+			numMap[v]--
+			num = append(num, v)
+
+		}
+	}
+	return num
+}
+
+func intersection2(nums1 []int, nums2 []int) []int {
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	num := make([]int, 0, 0)
+
+	for i, j := 0, 0; i < len(nums1) && j < len(nums2); {
+		if nums1[i] == nums2[j] {
+			if len(num) == 0 || num[len(num)-1] != nums1[i] {
+				num = append(num, nums1[i])
+			}
+			i++
+			j++
+		} else if nums1[i] > nums2[j] {
+			j++
+		} else {
+			i++
+		}
+
+	}
+
+	return num
+}
