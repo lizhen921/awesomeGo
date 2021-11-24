4882b53507a62008f293699b3413ac9c8aae27b3
Li Zhen <294474470@qq.com>
2021-11-24 23:29:53 +0800

    四数相加 II 利用map的解法

--- algorihtm/hashmap/hashmap_test.go
+++ algorihtm/hashmap/hashmap_test.go
@@ -152,3 +152,50 @@ func getNext(n int) int {
 func TestIsHappy(t *testing.T) {
 	fmt.Println(isHappy(19))
 }
+
+
+/*
+给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l) 能满足：
+
+- 0 <= i, j, k, l < n
+- nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+
+示例 1：
+
+输入：nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+输出：2
+解释：
+两个元组如下：
+1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) + (-1) + 2 = 0
+2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) + (-1) + 0 = 0
+
+示例 2：
+
+输入：nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+输出：1
+
+*/
+func fourSumCount(nums1 []int, nums2 []int, nums3 []int, nums4 []int) int {
+	sumMap := make(map[int]int)
+	for _,i := range nums1{
+		for _,j := range nums2{
+			sumMap[i+j] ++
+		}
+	}
+	count := 0
+	for _,k := range nums3{
+		for _,l := range nums4{
+			if _, ok := sumMap[-(k+l)]; ok{
+				count += sumMap[-(k+l)]
+			}
+		}
+	}
+	return count
+}
+func TestFourSumCount(t *testing.T)  {
+	num1 := []int{-1,-1}
+	num2 := []int{-1,1}
+	num3 := []int{-1,1}
+	num4 := []int{1,-1}
+	fmt.Println(fourSumCount(num1,num2,num3,num4))
+}
