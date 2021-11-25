51e17cfcd7fce70103c813624389ebc9cd0e0e00
Li Zhen <294474470@qq.com>
2021-11-25 22:20:24 +0800

    赎金信 map

--- algorihtm/hashmap/hashmap_test.go
+++ algorihtm/hashmap/hashmap_test.go
@@ -244,3 +244,37 @@ func TestTwoSum(t *testing.T) {
 	n := twoSum([]int{3, 3, 3}, 6)
 	fmt.Println(n)
 }
+
+/*
+为了不在赎金信中暴露字迹，从杂志上搜索各个需要的字母，组成单词来表达意思。
+
+给你一个赎金信 (ransomNote) 字符串和一个杂志(magazine)字符串，判断 ransomNote 能不能由 magazines 里面的字符构成。
+
+如果可以构成，返回 true ；否则返回 false 。
+
+magazine 中的每个字符只能在 ransomNote 中使用一次。
+
+示例 1：
+
+输入：ransomNote = "a", magazine = "b"
+输出：false
+
+*/
+func canConstruct(ransomNote string, magazine string) bool {
+	if len(ransomNote) > len(magazine){
+		return false
+	}
+	magMap := make(map[rune]int)
+	for _,v := range magazine{
+		magMap[v]++
+	}
+
+	for _,v := range ransomNote{
+		if m,ok := magMap[v];ok && m > 0 {
+			magMap[v] --
+		}else {
+			return false
+		}
+	}
+	return true
+}
