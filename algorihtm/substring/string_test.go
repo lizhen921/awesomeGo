4409cb2f33524dce918c8500a71a42f58ca5c4f0
lizhen <lizhen10762@autohome.com.cn>
2021-11-26 11:01:13 +0800

    按组反转字符串

--- algorihtm/substring/string_test.go
+++ algorihtm/substring/string_test.go
@@ -69,3 +69,40 @@ func TestReverseString(t *testing.T) {
 	reverseString(s)
 	fmt.Printf("%s", s)
 }
+
+/*
+反转字符串 II
+给定一个字符串 s 和一个整数 k，从字符串开头算起，每计数至 2k 个字符，就反转这 2k 字符中的前 k 个字符。
+
+如果剩余字符少于 k 个，则将剩余字符全部反转。
+如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+
+示例 1：
+
+输入：s = "abcdefg", k = 2
+输出："bacdfeg"
+示例 2：
+
+输入：s = "abcd", k = 2
+输出："bacd"
+
+*/
+func reverseStr(s string, k int) string {
+	sByte := []byte(s)
+
+	l := 0
+	for l < len(sByte) {
+		i := l
+		j := l + k - 1
+		if j > len(sByte)-1 {
+			j = len(sByte) - 1
+		}
+		for i < j {
+			sByte[i], sByte[j] = sByte[j], sByte[i]
+			i++
+			j--
+		}
+		l = l + 2*k
+	}
+	return string(sByte)
+}
