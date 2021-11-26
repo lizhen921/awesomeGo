dc9d05189dec863baf9c453c0c76a8374d69197a
lizhen <lizhen10762@autohome.com.cn>
2021-11-26 16:31:42 +0800

    按照单词反转字符串

--- algorihtm/substring/string_test.go
+++ algorihtm/substring/string_test.go
@@ -142,3 +142,58 @@ func replaceSpace(s string) string {
 	}
 	return string(b)
 }
+
+/*
+给你一个字符串s，逐个翻转字符串中的所有单词 。
+
+单词是由非空格字符组成的字符串。s中使用至少一个空格将字符串中的单词分隔开。
+
+请你返回一个翻转s中单词顺序并用单个空格相连的字符串。
+
+说明：
+
+输入字符串 s 可以在前面、后面或者单词间包含多余的空格。
+翻转后单词间应当仅用一个空格分隔。
+翻转后的字符串中不应包含额外的空格。
+
+
+示例 1：
+
+输入：s = "the sky is blue"
+输出："blue is sky the"
+示例 2：
+
+输入：s = "hello world "
+输出："world hello"
+解释：输入字符串可以在前面或者后面包含多余的空格，但是翻转后的字符不能包括。
+*/
+func reverseWords(s string) string {
+	res := ""
+	strB := []byte(s)
+	reverseString(strB)
+	l := len(strB)
+	i := 0
+
+	tempB := make([]byte, 0)
+	for i < l {
+		if strB[i] != ' ' {
+			tempB = append(tempB, strB[i])
+		}
+
+		if (strB[i] == ' ' || i == l-1) && len(tempB) > 0 {
+			reverseString(tempB)
+			res = res + " " + string(tempB)
+			i++
+			tempB = make([]byte, 0)
+			continue
+		}
+		i++
+	}
+	return res[1:]
+}
+
+func TestA(t *testing.T) {
+	s := "the sky is blue"
+	fmt.Println(reverseWords(s))
+
+}
