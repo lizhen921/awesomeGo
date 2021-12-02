d8254fa0c538bb6d4f7a956344f2c0a60853ea01
lizhen <lizhen10762@autohome.com.cn>
2021-12-02 15:24:37 +0800

    strStr查找字符串暴力算法

--- algorihtm/substring/string_test.go
+++ algorihtm/substring/string_test.go
@@ -243,3 +243,50 @@ func TestReverseLeftWords(t *testing.T) {
 
 	fmt.Println(reverseLeftWords1("lrloseumgh", 6))
 }
+
+/*
+实现 strStr() 函数。
+
+给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返回  -1 。
+
+说明：
+
+当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+
+对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与 C 语言的 strstr() 以及 Java 的 indexOf() 定义相符。
+
+示例 1：
+
+输入：haystack = "hello", needle = "ll"
+输出：2
+示例 2：
+
+输入：haystack = "aaaaa", needle = "bba"
+输出：-1
+示例 3：
+
+输入：haystack = "", needle = ""
+输出：0
+*/
+
+func strStr(haystack string, needle string) int {
+	if len(needle) > len(haystack) {
+		return -1
+	}
+	if needle == "" {
+		return 0
+	}
+
+	for i := 0; i < len(haystack); i++ {
+		j := 0
+		start := i
+		for j < len(needle) && start < len(haystack) && haystack[start] == needle[j] {
+			start++
+			j++
+		}
+		if j == len(needle) {
+			return i
+		}
+	}
+	return -1
+}
