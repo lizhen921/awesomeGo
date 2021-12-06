1ff7ce6d12072b8ee392ba901918d1e4279da340
lizhen <lizhen10762@autohome.com.cn>
2021-12-06 20:43:21 +0800

    逆波兰表达式求值

--- algorihtm/stackqueue/stackqueue_test.go
+++ algorihtm/stackqueue/stackqueue_test.go
@@ -1,6 +1,9 @@
 package stack
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
 type MyStack struct {
 	queue []int //创建一个队列
@@ -129,4 +132,66 @@ func removeDuplicates(s string) string {
 
 func TestRemoveDu(t *testing.T) {
 	removeDuplicates("abbaca")
+}
+
+/*
+逆波兰表达式求值
+
+根据 逆波兰表示法，求表达式的值。
+
+有效的算符包括 +、-、*、/ 。每个运算对象可以是整数，也可以是另一个逆波兰表达式。
+
+
+说明：
+
+整数除法只保留整数部分。
+给定逆波兰表达式总是有效的。换句话说，表达式总会得出有效数值且不存在除数为 0 的情况。
+
+示例 1：
+
+输入：tokens = ["2","1","+","3","*"]
+输出：9
+解释：该算式转化为常见的中缀算术表达式为：((2 + 1) * 3) = 9
+
+链接：https://leetcode-cn.com/problems/evaluate-reverse-polish-notation
+
+*/
+func evalRPN(tokens []string) int {
+	res := 0
+	if len(tokens) == 1 {
+		res, _ = strconv.Atoi(tokens[0])
+		return res
+	}
+	kMap := map[string]bool{"+": true, "-": true, "*": true, "/": true}
+
+	tempStack := make([]string, 0)
+	for _, v := range tokens {
+		if _, ok := kMap[v]; ok {
+			tempValueA, _ := strconv.Atoi(tempStack[len(tempStack)-2])
+			tempValueB, _ := strconv.Atoi(tempStack[len(tempStack)-1])
+			tempAB := 0
+			switch v {
+			case "+":
+				tempAB = tempValueA + tempValueB
+			case "-":
+				tempAB = tempValueA - tempValueB
+			case "*":
+				tempAB = tempValueA * tempValueB
+			case "/":
+				tempAB = tempValueA / tempValueB
+			default:
+				return -1
+			}
+			tempStack = tempStack[:len(tempStack)-2]
+			tempStack = append(tempStack, strconv.Itoa(tempAB))
+			res = tempAB
+		} else {
+			tempStack = append(tempStack, v)
+		}
+	}
+	return res
+}
+
+func TestEvalRpn(t *testing.T) {
+	evalRPN([]string{"4", "13", "5", "/", "+"})
 }
