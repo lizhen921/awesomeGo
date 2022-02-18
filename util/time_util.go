b473c3ca01d95eb402fb8d03671f59c6fd2f4009
lizhen <lizhen10762@autohome.com.cn>
2022-02-18 15:53:13 +0800

    util uuid

--- util/time_util.go
+++ util/time_util.go
@@ -1,6 +1,10 @@
 package util
 
-import "time"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 var stdOffset int64 = 8 * 3600 * 1e9
 
@@ -16,4 +20,17 @@ func IsToday(nano int64) bool {
 	currentTime := time.Now()
 	startTime := time.Date(currentTime.Year(), currentTime.Month(), currentTime.Day(), 0, 0, 0, 0, currentTime.Location()).Unix()
 	return (nano-startTime)/86400 < 1 && (nano-startTime) >= 0
+}
+
+func RandString32() string {
+	const kAlphaNum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	nanoStr := fmt.Sprintf("%d", time.Now().UnixNano())
+	if len(nanoStr) > 32 {
+		return nanoStr[0:32]
+	}
+	b := make([]byte, 32-len(nanoStr))
+	for i := range b {
+		b[i] = kAlphaNum[rand.Int63()%int64(len(kAlphaNum))]
+	}
+	return nanoStr + string(b)
 }
