4987f03c311eaa349f895018347b0d81430df8cc
lizhen <lizhen10762@autohome.com.cn>
2022-03-22 10:26:08 +0800

    获取本机ip

--- util/comm_util.go
+++ util/comm_util.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"math/rand"
+	"net"
 	"time"
 )
 
@@ -13,4 +14,21 @@ func GetRandInt(min, max int) int {
 	rand.Seed(int64(time.Now().Nanosecond())) //种子
 	var ret = rand.Intn(max + 1 - min)
 	return ret + min
+}
+
+//获取本机ip
+func getLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		// check the address type and if it is not a loopback the display it
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
 }
